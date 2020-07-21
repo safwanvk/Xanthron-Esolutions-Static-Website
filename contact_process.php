7c445bd06cffadfe4df888f172e2a52e2ade4737
safwanvk <safwanvalakundil@gmail.com>
2020-07-21 10:18:26 +0530

    :white_check_mark: Set Contact

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "example@mail.com";
+    $to = "muhammedsafwan250@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
